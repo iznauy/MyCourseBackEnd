060c6a2633c0fd36057c8496814b3b02d37ab4ee
iznauy <zy05160516@126.com>
2019-01-27 20:41:05 +0800

    init project

---
+++ src/main/java/top/nju/iznauy/MyCourseApplication.java
@@ -0,0 +1,14 @@
+package top.nju.iznauy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCourseApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/top/nju/iznauy/ServletInitializer.java
@@ -0,0 +1,14 @@
+package top.nju.iznauy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyCourseApplication.class);
+	}
+
+}
+
