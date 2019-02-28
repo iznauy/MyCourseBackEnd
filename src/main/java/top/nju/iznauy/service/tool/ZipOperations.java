9e2f363b3ddb8852c33cc3bcc9a900a0a28bc685
iznauy <zy05160516@126.com>
2019-02-28 21:46:44 +0800

    修复了压缩文件的时候的一个bug

--- src/main/java/top/nju/iznauy/service/tool/ZipOperations.java
+++ src/main/java/top/nju/iznauy/service/tool/ZipOperations.java
@@ -25,27 +25,29 @@ public class ZipOperations {
 
     private static void zip(String targetFile, String rawDirectory) throws IOException {
         File target = new File(targetFile);
-        if (!target.exists())
-            if (!target.createNewFile())
-                throw new RuntimeException("创建文件失败");
+        if (target.exists())
+            target.delete();
+        if (!target.createNewFile())
+            throw new RuntimeException("创建文件失败");
+
         ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(target));
-        BufferedOutputStream out = new BufferedOutputStream(zipOut);
         File rawDir = new File(rawDirectory);
         File[] items = rawDir.listFiles();
         for (File item : items) {
             zipOut.putNextEntry(new ZipEntry(item.getName()));
+            log.info("压缩文件：" + item.getName());
             BufferedInputStream in = new BufferedInputStream(new FileInputStream(item));
             int tag;
             while ((tag = in.read()) != -1)
-                out.write(tag);
+                zipOut.write(tag);
+            in.close();
         }
-        out.close();
         zipOut.close();
-        log.info("压缩完毕");
+        log.info("压缩完毕!");
     }
 
     public static String assignmentFolderToZip(int assignmentId) {
-        String result = null;
+        String result;
         try {
             Path path = Paths.get(ResourceUtils.getURL(StaticResourceConfig.ASSIGNMENT_BASE_PATH).getPath()).normalize();
             Path targetFile = path.resolve("" + assignmentId + ".zip").normalize();
